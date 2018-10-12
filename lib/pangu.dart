12dc424426b29cab66b99bb43edf86783da5f399
Edison <gn00742754@gmail.com>
2018-10-12 19:38:49 +0800

    check text is null

--- lib/pangu.dart
+++ lib/pangu.dart
@@ -49,6 +49,10 @@ class Pangu {
       '([A-Za-z0-9`~\$%\^&\*\-=\+\\\|/!;:,\.\?\u00a1-\u00ff\u2022\u2026\u2027\u2150-\u218f])([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])');
 
   String spacingText(String text) {
+    if (text == null) {
+      return "";
+    }
+
     String newText = text;
 
     newText =
